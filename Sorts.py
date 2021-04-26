fd94db45ee3bc90c26297602f003a845c92da661
Arthur-Meow <arthurzhong9494@outlook.com>
2021-04-25 22:35:29 -0400

    Add more sorts

--- Sorts.py
+++ Sorts.py
@@ -1,4 +1,5 @@
 ### Bubble sort: compare 2 elements and switch them into correct order
+### n^2
 def bubbleSort(alist):
     for i in range(len(alist)-1, 0, -1):
         for j in range(i):
@@ -24,6 +25,7 @@ def shortBubbleSort(alist):
 
 
 ### Select sort: find the largest one and place it at the end
+### n^2
 def selectSort(alist):
     for i in range(len(alist)-1, 0, -1):
         position = 0
@@ -38,6 +40,7 @@ def selectSort(alist):
 
 
 ### Insert sort: treat the left part as a list, insert the number into the correct place
+### n^2
 def insertSort(alist):
     for i in range(1, len(alist)):
         pos = i
@@ -49,3 +52,90 @@ def insertSort(alist):
         alist[pos] = value
     
     return alist
+
+
+### Merge sort: Keep breaking down the list into two parts, sort the sublist then merge the sorted sublists
+### nlog(n)
+def mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        leftpart = alist[:mid]
+        rightpart = alist[mid:]
+        mergeSort(leftpart)
+        mergeSort(rightpart)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(leftpart) and j < len(rightpart):
+            if leftpart[i] < rightpart[j]:
+                alist[k] = leftpart[i]
+                i += 1
+            else:
+                alist[k] = rightpart[j]
+                j += 1
+            k += 1
+
+        while i < len(leftpart):
+            alist[k] = leftpart[i]
+            i += 1
+            k += 1
+
+        while j < len(rightpart):
+            alist[k] = rightpart[j]
+            j += 1
+            k += 1
+    return alist
+            
+
+#################    Quick Sort   #############################
+### Quick sort (use middle number): find the 2nd largest number in three numbers that are picked from the most left, middle,
+### and most right of the list, place it to the most left position, then perform quick sort.
+### nlog(n)
+def setPivot(alist, first, last):
+    # first and last are positions
+    mid = (first + last) // 2
+    if alist[mid] > alist[last]:
+        alist[mid], alist[last] = alist[last], alist[mid]
+
+    if alist[first] > alist[last]:
+        alist[first], alist[last] = alist[last], alist[first]
+
+    if alist[first] < alist[mid]:
+        alist[first], alist[mid] = alist[mid], alist[first]
+
+    return alist[first]
+
+def partition(alist, first, last):
+    pivot = setPivot(alist, first, last)
+    leftpos = first + 1
+    rightpos = last
+    done = False
+
+    while not done:
+        while leftpos <= rightpos and alist[leftpos] <= pivot:
+            leftpos += 1
+
+        while leftpos <= rightpos and alist[rightpos] >= pivot:
+            rightpos -= 1
+
+        if leftpos > rightpos:
+            done = True
+        else:
+            alist[leftpos], alist[rightpos] = alist[rightpos], alist[leftpos]
+
+    alist[first], alist[rightpos] = alist[rightpos], alist[first]
+
+    return rightpos
+
+def quickSortHelper(alist, first, last):
+    if first < last:
+        splitPoint = partition(alist, first, last)
+        quickSortHelper(alist, first, splitPoint-1)
+        quickSortHelper(alist, splitPoint+1, last)
+
+def quickSort(alist):
+    quickSortHelper(alist, 0, len(alist)-1)
+    return alist
+
